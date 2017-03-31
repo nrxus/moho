ae43a2a4fe4e29a2112ee9ceec21fd925b1c7777
Andres <andresnrx@gmail.com>
2017-03-31 02:25:03 -0700

    figured out how to not require borrowing &mut self for 'ttf

--- examples/button.rs
+++ examples/button.rs
@@ -15,8 +15,7 @@ pub struct MainGame<'ttf,
     input_manager: InputManager<E>,
     resource_manager: ResourceManager<R>,
     renderer: R,
-    font_loader: F,
-    _marker: std::marker::PhantomData<&'ttf F>,
+    font_loader: &'ttf F,
 }
 
 impl<'ttf, E, F, R> MainGame<'ttf, E, F, R>
@@ -24,18 +23,17 @@ impl<'ttf, E, F, R> MainGame<'ttf, E, F, R>
           F: FontLoader<'ttf>,
           R: ResourceLoader + Renderer + FontTexturizer<'ttf, F>
 {
-    pub fn new(renderer: R, input_manager: InputManager<E>, font_loader: F) -> Self {
+    pub fn new(renderer: R, input_manager: InputManager<E>, font_loader: &'ttf F) -> Self {
         let resource_manager = ResourceManager::new();
         MainGame {
             input_manager: input_manager,
             resource_manager: resource_manager,
             renderer: renderer,
             font_loader: font_loader,
-            _marker: std::marker::PhantomData,
         }
     }
 
-    pub fn run(&'ttf mut self) -> Result<()> {
+    pub fn run(&mut self) -> Result<()> {
         let image = self.resource_manager.load_texture("examples/background.png", &self.renderer)?;
         let font = self.font_loader.load("examples/fonts/kenpixel_mini.ttf", 48)?;
         let mut timer = Timer::new();
@@ -67,6 +65,6 @@ fn main() {
     const WINDOW_HEIGHT: u32 = 720;
     let (renderer, input_manager) = moho::init("MohoGame", WINDOW_WIDTH, WINDOW_HEIGHT).unwrap();
     let loader = sdl2::ttf::init().chain_err(|| "cannot init loader").unwrap();
-    let mut game = MainGame::new(renderer, input_manager, loader);
+    let mut game = MainGame::new(renderer, input_manager, &loader);
     game.run().unwrap();
 }
