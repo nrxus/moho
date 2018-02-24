660016b1ffaaa4afb1c379b9ad0faeea7810f94a
Andres <andresnrx@gmail.com>
2018-02-24 09:43:18 -0800

    use my own fork of synstructure

--- moho-derive/src/lib.rs
+++ moho-derive/src/lib.rs
@@ -1,73 +1,28 @@
 extern crate proc_macro;
-#[macro_use]
 extern crate quote;
 #[macro_use]
 extern crate syn;
+#[macro_use]
+extern crate synstructure;
 
-use proc_macro::TokenStream;
-use syn::{punctuated, Data, DeriveInput, Fields, Generics, PredicateType, TypeGenerics,
-          TypeParamBound, WhereClause, WherePredicate};
-use quote::ToTokens;
+decl_derive!([Show] => show_derive);
 
-#[proc_macro_derive(Show)]
-pub fn derive_show(input: TokenStream) -> TokenStream {
-    let input: DeriveInput = syn::parse(input).unwrap();
-    let name = input.ident;
-    let body = body(&input.data);
-    let (impl_generics, ty_generics, where_clause) = generics(&input.generics);
-
-    let expanded = quote! {
-        impl #impl_generics ::moho::renderer::Show<R> for #name #ty_generics #where_clause {
-            fn show(&self, renderer: &mut R) -> ::moho::Result<()> {
-                #body
-            }
+fn show_derive(mut structure: synstructure::Structure) -> quote::Tokens {
+    let body = structure.each(|bi| {
+        quote! {
+            renderer.show(#bi)?;
         }
-    };
-    expanded.into()
-}
-
-fn generics(generics: &Generics) -> (quote::Tokens, TypeGenerics, quote::Tokens) {
-    let param: syn::GenericParam = parse_quote!(R: ::moho::renderer::Renderer);
-    let mut gen_clone = generics.clone();
-    gen_clone.params.push(param);
-    let impl_generics = gen_clone.split_for_impl().0;
-    let (_, ty_generics, where_clause) = generics.split_for_impl();
-    let mut where_clause = where_clause.cloned().unwrap_or_else(|| WhereClause {
-        predicates: punctuated::Punctuated::new(),
-        where_token: Default::default(),
     });
-    where_clause
-        .predicates
-        .push(WherePredicate::Type(PredicateType {
-            lifetimes: None,
-            colon_token: Default::default(),
-            bounded_ty: parse_quote!(T),
-            bounds: Some(punctuated::Pair::End(TypeParamBound::Trait(parse_quote!(
-                ::moho::renderer::Draw<R>
-            )))).into_iter()
-                .collect(),
-        }));
-    (
-        impl_generics.into_tokens(),
-        ty_generics,
-        where_clause.into_tokens(),
-    )
-}
-
-fn body(data: &Data) -> quote::Tokens {
-    match *data {
-        Data::Struct(ref data) => match data.fields {
-            Fields::Named(ref fields) => {
-                let names = fields.named.iter().map(|f| f.ident);
-                quote! {
-                    #(renderer.show(&self.#names)?;)*
+    structure
+        .add_impl_generic(parse_quote!(R: ::moho::renderer::Renderer))
+        .bounds(synstructure::BoundsToAdd::Fields)
+        .bound_impl(
+            quote!(::moho::renderer::Show<R>),
+            quote!{
+                fn show(&self, renderer: &mut R) -> ::moho::Result<()> {
+                    match *self { #body }
                     Ok(())
                 }
-            }
-            Fields::Unnamed(_) => unimplemented!("Struct::Unnamed"),
-            Fields::Unit => unimplemented!("Struct::Unit"),
-        },
-        Data::Enum(_) => unimplemented!("Enum"),
-        Data::Union(_) => unimplemented!("Union"),
-    }
+            },
+        )
 }
--- moho-derive/tests/test.rs
+++ moho-derive/tests/test.rs
@@ -1,10 +1,6 @@
 extern crate moho;
 #[macro_use]
 extern crate moho_derive;
-#[macro_use]
-extern crate synstructure;
-
-use moho::texture::Image;
 
 mod foo {
     use moho::texture::Image;
@@ -13,5 +9,6 @@ mod foo {
     #[derive(Show)]
     struct Assets<T> {
         image: Image<T>,
+        other: Rc<T>,
     }
 }
